e4902737fa1925d805841958f915c13ce95c59ed
CNGamboardz <138053722+CNGamboardz@users.noreply.github.com>
2023-09-08 12:20:04 -0600

    ArrelosPython-GREA

---
+++ Arreglos.py
@@ -0,0 +1,10 @@
+#Hacer un programa de arreglos
+#Erick Gamboa
+
+NUM = 8
+nums = [0] * NUM 
+total = 0
+for i in range(NUM):
+        nums[i] = int(input("Por favor, introduzca el numero: "))
+        total += nums[i]
+        print("El total de numeros es: ", total)
